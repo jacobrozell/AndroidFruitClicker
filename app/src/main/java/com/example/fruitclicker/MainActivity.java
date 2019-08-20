5b11361a03bcb71cf7212347116eecfaf4372be5
Jacob Rozell <jacob.rozell83@gmail.com>
2019-08-19 23:30:39 -0500

    Added apple animation

--- app/src/main/java/com/example/fruitclicker/MainActivity.java
+++ app/src/main/java/com/example/fruitclicker/MainActivity.java
@@ -4,28 +4,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
     TextView scoreLabel;
     Button clickButton;
+    ImageView appleImage;
+
+    int[] appleImageArray = {
+            R.drawable.apple0,
+            R.drawable.apple1,
+            R.drawable.apple2,
+            R.drawable.apple3,
+            R.drawable.apple4,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        scoreLabel = (TextView) findViewById(R.id.scoreLabel);
-        clickButton = (Button) findViewById(R.id.clickMeButton);
-
+        scoreLabel = findViewById(R.id.scoreLabel);
+        clickButton = findViewById(R.id.clickMeButton);
+        appleImage = findViewById(R.id.imageView);
+        appleImage.setImageResource(R.drawable.apple0);
 
         clickButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 score++;
+                int calc = score % 5;
+                appleImage.setImageResource(appleImageArray[calc]);
+
                 scoreLabel.setText(String.valueOf(score));
+
             }
         });
     }
